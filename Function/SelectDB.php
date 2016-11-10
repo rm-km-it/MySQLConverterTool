748e314c5af2b077ad4cf9fa50b73d4577bf0fc7
raimund <rm@km-it.de>
2016-11-10 11:28:11 +0100

    replaced mysql_select_db() with mysqli_select_db() instead of emulating with USE statement

--- Function/SelectDB.php
+++ Function/SelectDB.php
@@ -16,7 +16,7 @@ require_once('Generic.php');
 class MySQLConverterTool_Function_SelectDB extends MySQLConverterTool_Function_Generic {
   
     
-    public $new_name = 'mysqli_query';
+    public $new_name = 'mysqli_select_db';
 
     
     public function __construct() {   
@@ -37,18 +37,18 @@ class MySQLConverterTool_Function_SelectDB extends MySQLConverterTool_Function_G
             
         list($db, $db_type) = $this->extractValueAndType(trim($db));            
         if ('const' == $db_type) {
-            $ret = sprintf('((bool)mysqli_query(%s, "USE " . constant(\'%s\')))', $conn, $db);
+            $ret = sprintf('mysqli_select_db(%s, constant(\'%s\'))', $conn, $db);
         } else {
-            $ret = sprintf('((bool)mysqli_query(%s, "USE " . %s))', $conn, $db);
+            $ret = sprintf('mysqli_select_db(%s, %s)', $conn, $db);
         }
         
-        return array('mysql_select_db(string database_name [...]) is emulated using mysqli_query() and USE database_name. This is a possible SQL injection security bug as no tests are performed what value database_name has. Check your script!', $ret);
+        return array('', $ret);
     }
     
     
     function getConversionHint() {
         
-        return 'Emulated using mysqli_query() and USE.';
+        return 'Converted to mysqli_select_db().';
     }
 
 
